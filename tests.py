1ed2e7b5bdfc4158ab09d4c030160d15d5068d86
Cornand Benjamin Maël <benjamin.cornand@yahoo.fr>
2019-11-23 16:14:39 +0100

    corriger les tests

--- tests.py
+++ tests.py
@@ -11,25 +11,25 @@ def assertTrue(val1,val2,raison):
 
 def test_nombre_disques():
     plat = init(5)
-    assertTrue(nombre_disques(plat,0),0,"tour vide")
-    assertTrue(nombre_disques(plat,1),5, "tour remplie a 5")
+    assertTrue(nombre_disques(plat,0),5,"tour vide")
+    assertTrue(nombre_disques(plat,1),0, "tour remplie a 5")
 
 def test_disque_supérieur():
     plat = init(5)
-    assertTrue(disque_superieur(plat,0), -1, "tour vide")
-    assertTrue(disque_superieur(plat,1), 5, "tour remplie")
+    assertTrue(disque_superieur(plat,1), -1, "tour vide")
+    assertTrue(disque_superieur(plat,0), 5, "tour remplie")
 
 def test_position_disque():
     plat = init(5)
-    assertTrue(position_disque(plat,1),1,"position initiale")
-    assertTrue(position_disque(plat,2),1, "pos initiale")
+    assertTrue(position_disque(plat,1),0,"position initiale")
+    assertTrue(position_disque(plat,2),0, "pos initiale")
 
 def test_verifier_deplacement():
     plat = init(5)
-    assertTrue(verifier_deplacement(plat,0,1), False, "vide sur plein")
-    assertTrue(verifier_deplacement(plat,0,2),False, "vide sur vide")
-    assertTrue(verifier_deplacement(plat,1,2),True, "plein sur vide")
-    assertTrue(verifier_deplacement(plat,1,1),True, "plein sur plein")
+    assertTrue(verifier_deplacement(plat,2,0), False, "vide sur plein")
+    assertTrue(verifier_deplacement(plat,1,2),False, "vide sur vide")
+    assertTrue(verifier_deplacement(plat,0,2),True, "plein sur vide")
+    assertTrue(verifier_deplacement(plat,0,0),False, "plein sur plein")
 
 def test_verifier_victoire():
     plat = init(5)
