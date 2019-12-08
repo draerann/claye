9719644c73793a0e658c6aa146b6207329cab5fe
Cornand Benjamin Maël <benjamin.cornand@yahoo.fr>
2019-12-08 13:42:02 +0100

    maj de B

--- B.py
+++ B.py
@@ -1,72 +1,17 @@
+
 from turtle import *
-
-def init(n):
-    Tour0=[]
-    Tour1=[]
-    Tour2=[]
-
-    Tour0=list(range (1,n+1))
-    Plateau=[Tour0,Tour1,Tour2]
-    return Plateau
-
-
-def nombre_disques(plateau,numtour):
-    c=len(plateau[numtour])
-    return c
-
-
-def disque_superieur(plateau,numtour):
-    c=nombre_disques(plateau,numtour)
-    if c ==0:
-        return-1
-    else:
-        return plateau[numtour][c-1]
-    #l=plateau[0]
-    #n-l[C]
-    #return c
-
-
-    
-def position_disque(plateau, numdisque):
-    for l in range (0,3):
-        print(l)
-        plateau[l]
-        try:
-            position=plateau[l].index(numdisque)
-            return l
-        except ValueError:
-            continue   
-
-def verifier_deplacement(plateau,nt1,nt2):
-    #on veut verifier que le dique deplacé soit plus petit que celui sur lequel on veut le placer
-    d1=disque_superieur(plateau,nt1)
-    d2=disque_superieur(plateau,nt2)
-    if d1<0:
-        return False
-    if d2<0:
-        return True
-    return d1<d2
-
-
-def verifier_victoire(plateau,n):
-    jotaro=plateau[2]
-    isDecroissant=jotaro==list(reversed(sorted(jotaro)))
-    #sort() met dans l'ordre croissant reverse()les met dans l'ordre décroissant
-    return n==len(jotaro)and isDecroissant
-
-
-Tour0=[2,1]
-Tour1=[]
-Tour2=[3]
-p=[Tour0,Tour1,Tour2]
+from A import *
 
 setup(1500,600)
 speed(0)
+
 def dessine_plateau(n):
+    #On place le curseur et fixe les valeurs utiles
     up()
     goto(-600,0)
     down()
     n2=(n-1)*30+40
+    #On trace le plateau
     for i in range(0,4,1):
         if i%2==0:
             forward(3*n2+4*20)            
@@ -74,6 +19,7 @@ def dessine_plateau(n):
             forward(20)            
         left(90)
     up()
+    #Puis les 3 tours
     for i in range(0,3,1):
         x=(1+i)*20+(1+i*2)*(n2/2)-3
         goto(x-600,20)
@@ -87,6 +33,7 @@ def dessine_plateau(n):
         up()
 
 def dessine_disque(nd,plateau,n):
+    #On fixe les valeurs qui nous servirons à placer le disque
     color("black")
     t=position_disque(plateau,nd)
     if t==0:
@@ -95,9 +42,10 @@ def dessine_disque(nd,plateau,n):
         r=Tour1.index(nd)
     elif t==2:
         r=Tour2.index(nd)
-    print(t,r)
+    #On se place en bas à gauche du disque    
     goto((t+1)*20+(((n-1)*30+40)/2)*(t*2+1)-600-((nd-1)*30+40)/2,r*20+20)
     down()
+    #On trace le disque
     for i in range(0,4,1):
             if i%2==0:
                 forward((nd-1)*30+40)
@@ -105,6 +53,7 @@ def dessine_disque(nd,plateau,n):
                 forward(20)
             left(90)
     forward((nd-1)*15+17)
+    #On efface la tour derrière
     for i in range(0,4,1):
         if i%2==0:
             forward(6)            
@@ -119,53 +68,58 @@ def dessine_disque(nd,plateau,n):
     forward(-nd*15-17)
 
 def efface_disque(nd,plateau,n):
-    l20=[0,2,4,6]
-    lef=[0,1,4,5]
+    #Parallèlement à dessine disque, on récupère là hauteur, le décalage à droite où a gauche
+    seth(0)
     t=position_disque(plateau,nd)
     if t==0:
         r=Tour0.index(nd)    
     elif t==1:
         r=Tour1.index(nd)
     elif t==2:
-        r=Tour2.index(nd)
-    print(t,r)
-    goto((t+1)*20+(((n-1)*30+40)/2)*(t*2+1)-600-((nd-1)*30+40)/2,r*20+20)
+        r=Tour2.index(nd)    
+    #On efface les trois segements superieur du disque (le dernier étant confondu avec le disque inférieur)
+    goto((t+1)*20+(((n-1)*30+40)/2)*(t*2+1)-600-((nd-1)*30+40)/2,r*20+21)
     left(90)
-    for i in range(0,6,1):
-        color("white")
-        if i==2 or i==4:
-            color("black")
-        if i in l20:
-            forward(20)
-        if i==1 or i==5:
-            forward((nd-1)*15+17)
-        if i==3:
-            forward(6)
-        if i in lef:
-            right(90)
-        if i==2 or i==3:
-            right(90)       
-        
+    pencolor("white")
+    down()
+    forward(19)
+    right(90)
+    forward((nd-1)*30+40)
+    right(90)
+    forward(20)
+    up()
+    #Puis on retrace les deux segements de la tour
+    goto((t+1)*20+(((n-1)*30+40)/2)*(t*2+1)-600-((nd-1)*30+40)/2+((nd-1)*30+40)/2-3,r*20+21)
+    right(180)
+    color("black")
+    down()
+    forward(20)
+    up()
+    goto((t+1)*20+(((n-1)*30+40)/2)*(t*2+1)-600-((nd-1)*30+40)/2+((nd-1)*30+40)/2+3,r*20+21)
+    down()
+    forward(20)
+    up()
+
 def dessine_config(plateau,n):
+    #On dessine tout les disques, du plus grand au plus petit
     dessine_plateau(n)
     for i in range(n,0,-1):
-        dessine_disque(i)
+        dessine_disque(i,plateau,n)
 
 def efface_tout(plateau,n):
-    for i in range(n,0,-1):
-        efface_disque(i,plateau,n)
-    color("white")
-    dessine_plateau(n)
-    
-    
-    
-
-dessine_plateau(3)
-
-dessine_disque(2,p,3)
-dessine_disque(1,p,3)
-dessine_disque(3,p,3)
-
-efface_disque(2,p,3)
-
-exitonclick()
+    #Je trace un grand cadre blanc de 10 de coté de plus que le plateau, que je remplis de blanc
+    pencolor("white")
+    fillcolor("white")
+    begin_fill()
+    up()
+    goto(-610,-10)
+    down()
+    seth(0)
+    for i in range(0,5,1):
+        if i%2==0:
+            forward(3*((n-1)*30+40)+5*20)
+            left(90)    
+        if i%2!=0:
+            forward(20*n+80)
+            left(90)
+    end_fill()
--- C.py
+++ C.py
@@ -7,7 +7,11 @@ def lire_coords(plateau):
         if ntourd>2 or ntourd<0:
             t=False
             print("valeur interdite")
-        elif:
+        ntoura=int(input("numéro de la tour de d'arrivée")) 
+        if ntoura>2 or ntoura<0:
+            t=False
+            print("valeur interdite")
+        else:
             for i in range (0,3):
                 if (verifier_deplacement(plateau,ntourd,i):
                     t=True
@@ -31,12 +35,22 @@ def jouer_un_coup(plateau,n):
     (ntourd,ntoura) = lire_coords(plateau)
     nd=disque_supérieur (plateau,ntourd)
     efface_disque(nd,plateau,n)
-    plateau[ntourd].remove(ndisque)
+    plateau[ntourd].remove(nd)
     nd=disque_supérieur (plateau,ntoura)
     dessine_disque(nd,plateau,n)
     plateau[ntourd].remove(ndisque)
-    liste.append(nd)
+    plateau[ntoura].append(nd)
 
 def boucle_jeu(plateau,n):
+    p=0
+    nmax=50
+    while !(verifier-victoire(plateau,n)):
+        jouer_un_coup(plateau,n)
+        if p>nmax:
+            print("perdu trop de coups")
+        jouer_un_coup(plateau,n)
+        p+=1
     
+return p
+
     
